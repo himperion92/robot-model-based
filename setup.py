09f1ec1398ca1a997a25039e8421abddd368334a
himperion92 <amontescobo@gmail.com>
2020-04-26 14:40:32 +0200

    update demo graph

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from robot_model_based import __version__
 
 setup(
-    name="HelloWorld",
+    name="robot-model-based",
     version=__version__,
     packages=find_packages(),
 )
