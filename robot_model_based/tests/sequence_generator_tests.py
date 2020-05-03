caa6f6d68db2e2f4126ae65f6d73e83798160164
himperion92 <amontescobo@gmail.com>
2020-05-03 20:00:38 +0200

    fix unit test for sequence_generator

--- robot_model_based/tests/sequence_generator_tests.py
+++ robot_model_based/tests/sequence_generator_tests.py
@@ -14,31 +14,10 @@ class SequenceGeneratorTest(unittest.TestCase):
         logging.basicConfig(level=logging.DEBUG)
         self._seq_gen = SequenceGenerator()
 
-    @mock.patch('robot_model_based.sequence_generator.SequenceGenerator.'
-                '_decode_graph')
-    @mock.patch('robot_model_based.sequence_generator.SequenceGenerator.'
-                '_generate_random_sequence')
-    def test_generate_sequences_random(self, mock_gen_seq, mock_decode_graph):
-        file_path = '/home/bob/workspace/graph.grahpml'
-        decoded_graph = mock.Mock()
-        decoded_graph.node = {'node_a': 'a', 'node_b': 'b'}
-        decoded_graph.edge = {'edge_a': 'a', 'edge_b': 'b'}
-        mock_decode_graph.side_effect = [decoded_graph]
-        mock_gen_seq.side_effect = [['1', '2', '3']]
-        nodes, edges, exec_seqs = self._seq_gen.generate_sequences(file_path,
-                                                                   'random',
-                                                                   coverage=45)
-        self.assertEqual({'node_a': 'a', 'node_b': 'b'}, nodes)
-        self.assertEqual({'edge_a': 'a', 'edge_b': 'b'}, edges)
-        self.assertEqual(['1', '2', '3'], exec_seqs)
-        mock_decode_graph.assert_called_once_with(file_path)
-        mock_gen_seq.assert_called_once_with(decoded_graph, 45)
-
     @mock.patch('networkx.readwrite.graphml.read_graphml')
     @mock.patch('networkx.all_simple_paths')
     def test_generate_sequences_full(self, mock_simple_paths, mock_read_graphml):
         dummy_file_path = '/home/bob/workspace/graph.grahpml'
-        
         dummy_nodes = {'n0': {'x': '0', 'y':'1', 'label':'state_a'}, 
                        'n1': {'x': '2', 'y':'3', 'label':'state_b'},
                        'n2': {'x': '4', 'y':'5', 'label':'state_c'},
@@ -59,15 +38,48 @@ class SequenceGeneratorTest(unittest.TestCase):
                                          [['n0', 'n1', 'n3']]]
         nodes, edges, exec_seqs = self._seq_gen.generate_sequences(dummy_file_path,
                                                                    'full')
+        mock_read_graphml.assert_called_once_with(dummy_file_path)
         self.assertEqual(dummy_nodes, nodes)
         self.assertEqual(dummy_edges, edges)
         self.assertEqual(expected_exec_seq, exec_seqs)
-        mock_read_graphml.assert_called_once_with(dummy_file_path)
 
+    @mock.patch('networkx.number_of_nodes')
+    @mock.patch('networkx.readwrite.graphml.read_graphml')
     @mock.patch('robot_model_based.sequence_generator.SequenceGenerator.'
-                '_decode_graph')
+                '_generate_full_sequence')
+    def test_generate_sequences_coverage(self, mock_gen_seq,
+                                         mock_read_graphml,
+                                         mock_num_of_nodes):
+        dummy_file_path = '/home/bob/workspace/graph.grahpml'
+        dummy_nodes = {'n0': {'x': '0', 'y':'1', 'label':'state_a'}, 
+                       'n1': {'x': '2', 'y':'3', 'label':'state_b'},
+                       'n2': {'x': '4', 'y':'5', 'label':'state_c'},
+                       'n3': {'x': '6', 'y':'7', 'label':'state_d'}}
+        dummy_edges = [('n0', 'n1', {'label':'action_1', 'id':'e1'}),
+                       ('n1', 'n0', {'label':'action_2', 'id':'e2'}),
+                       ('n1', 'n2', {'label':'action_3', 'id':'e3'}),
+                       ('n1', 'n3', {'label':'action_4', 'id':'e4'})] 
+        expected_exec_seq = [['n0', 'n1', 'n3']]
+        mock_num_of_nodes.side_effect = [len(dummy_nodes)]
+        decoded_graph = mock.Mock()
+        decoded_graph.nodes.return_value = dummy_nodes
+        decoded_graph.edges.return_value = dummy_edges
+        mock_read_graphml.side_effect = [decoded_graph]
+        mock_gen_seq.side_effect = [[['n0', 'n1', 'n0'],
+                                     ['n0','n1','n2'],
+                                     ['n0','n1','n3']]]
+        nodes, edges, exec_seqs = self._seq_gen.generate_sequences(dummy_file_path,
+                                                                   'random',
+                                                                   coverage=1)
+        mock_read_graphml.assert_called_once_with(dummy_file_path)
+        mock_gen_seq.assert_called_once_with(decoded_graph)
+        self.assertEqual(dummy_nodes, nodes)
+        self.assertEqual(dummy_edges, edges)
+        self.assertEqual(expected_exec_seq, exec_seqs)
+
+    @mock.patch('networkx.readwrite.graphml.read_graphml')
     @mock.patch('robot_model_based.sequence_generator.SequenceGenerator.'
-                '_generate_random_sequence')
+                '_generate_full_sequence')
     def test_generate_sequences_wrong_strategy(self, mock_gen_seq,
                                                mock_decode_graph):
         file_path = '/home/bob/workspace/graph.grahpml'
@@ -83,11 +95,3 @@ class SequenceGeneratorTest(unittest.TestCase):
 
         mock_decode_graph.assert_not_called()
         mock_gen_seq.assert_not_called()
-
-    @mock.patch('networkx.readwrite.graphml.read_graphml')
-    def test_decode_graph(self, mock_read_graphml):
-        file_path = '/home/bob/workspace/graph.grahpml'
-        mock_read_graphml.side_effect = [1]
-        graph = self._seq_gen._decode_graph(file_path)
-        mock_read_graphml.assert_called_once_with(file_path)
-        self.assertEqual(1, graph)
